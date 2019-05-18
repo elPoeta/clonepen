a3b040fd85343e8305f904592aa0d59933e7ec9a
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2019-05-18 18:36:03 -0300

    add compiler for html css and javascript change min library for full

--- js/app.js
+++ js/app.js
@@ -1,47 +1,72 @@
 ace.require("ace/ext/language_tools");
 
 window.editorHtml = ace.edit("editorHtml", {
-    mode: "ace/mode/html",
-    selectionStyle: "text"
+  mode: "ace/mode/html",
+  selectionStyle: "text"
 });
-
 
 editorHtml.setTheme("ace/theme/monokai");
 
 editorHtml.setOption("mergeUndoDeltas", "always");
 
 editorHtml.setOptions({
-    autoScrollEditorIntoView: true,
-    enableBasicAutocompletion: true,
-    enableSnippets: true,
-    enableLiveAutocompletion: true,
-    fontSize: "100%"
+  autoScrollEditorIntoView: true,
+  enableBasicAutocompletion: true,
+  enableSnippets: true,
+  enableLiveAutocompletion: true,
+  fontSize: "100%"
 });
 
-editorHtml.getSession().on('change', function () {
-    compile();
+editorHtml.getSession().on("change", function() {
+  compile();
 });
 
-/*
 window.editorCss = ace.edit("editorCss", {
-    mode: "ace/mode/css",
-    selectionStyle: "text"
+  mode: "ace/mode/css",
+  selectionStyle: "text"
+});
+
+editorCss.setTheme("ace/theme/monokai");
+
+editorCss.setOption("mergeUndoDeltas", "always");
+
+editorCss.setOptions({
+  autoScrollEditorIntoView: true,
+  enableBasicAutocompletion: true,
+  enableSnippets: true,
+  enableLiveAutocompletion: true,
+  fontSize: "100%"
+});
+
+editorCss.getSession().on("change", function() {
+  compile();
 });
 
 window.editorJs = ace.edit("editorJs", {
-    mode: "ace/mode/javascript",
-    selectionStyle: "text"
+  mode: "ace/mode/javascript",
+  selectionStyle: "text"
 });
 
-
-editorCss.setTheme("ace/theme/monokai");
 editorJs.setTheme("ace/theme/monokai");
-*/
+
+editorJs.setOption("mergeUndoDeltas", "always");
+
+editorJs.setOptions({
+  autoScrollEditorIntoView: true,
+  enableBasicAutocompletion: true,
+  enableSnippets: true,
+  enableLiveAutocompletion: true,
+  fontSize: "100%"
+});
+
+editorJs.getSession().on("change", function() {
+  compile();
+});
 
 const compile = () => {
-    const result = document.querySelector('#codeResult').contentWindow.document;
+  const result = document.querySelector("#codeResult");
 
-    result.open();
-    result.writeln(editorHtml.getValue());
-    result.close();
-}
+  result.srcdoc = `${editorHtml.getValue()}
+                  <style>${editorCss.getValue()}</style>
+                  <script>${editorJs.getValue()}</script>`;
+};
