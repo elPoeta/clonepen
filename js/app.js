9dd39b230e10754fc6b7781f3fee48641df5b69c
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2019-05-18 17:43:37 -0300

    setup html editor

--- js/app.js
+++ js/app.js
@@ -1,8 +1,28 @@
+ace.require("ace/ext/language_tools");
+
 window.editorHtml = ace.edit("editorHtml", {
     mode: "ace/mode/html",
     selectionStyle: "text"
 });
 
+
+editorHtml.setTheme("ace/theme/monokai");
+
+editorHtml.setOption("mergeUndoDeltas", "always");
+
+editorHtml.setOptions({
+    autoScrollEditorIntoView: true,
+    enableBasicAutocompletion: true,
+    enableSnippets: true,
+    enableLiveAutocompletion: true,
+    fontSize: "100%"
+});
+
+editorHtml.getSession().on('change', function () {
+    compile();
+});
+
+/*
 window.editorCss = ace.edit("editorCss", {
     mode: "ace/mode/css",
     selectionStyle: "text"
@@ -13,6 +33,15 @@ window.editorJs = ace.edit("editorJs", {
     selectionStyle: "text"
 });
 
-editorHtml.setTheme("ace/theme/monokai");
+
 editorCss.setTheme("ace/theme/monokai");
 editorJs.setTheme("ace/theme/monokai");
+*/
+
+const compile = () => {
+    const result = document.querySelector('#codeResult').contentWindow.document;
+
+    result.open();
+    result.writeln(editorHtml.getValue());
+    result.close();
+}
